6cb54e619e0bb0c71599bcb5f44376bcaa613013
AnmolPatil2 <anmol.patil004@gmail.com>
2019-05-18 21:08:31 +0530

    events mousemove

--- app.js
+++ app.js
@@ -2,12 +2,23 @@ new Vue({
     el: '#app',
     data: {
         message: 'whats uppp',
-        value: 10
+        value: 10,
+        coords: {
+            x: 0,
+            y: 0
+
+        }
     },
     methods: {
         greet() {
             this.value++
+        }, logEvent(e) {
+            console.log(e);
         },
+        logCoords(e) {
+            this.coords.x = e.offsetX
+            this.coords.y = e.offsetY
+        }
     }
 
 })
