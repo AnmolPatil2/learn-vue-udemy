59956f4f953598fd106528b0c5a8326a0783e284
AnmolPatil2 <anmol.patil004@gmail.com>
2019-05-18 21:40:51 +0530

    events key up

--- app.js
+++ app.js
@@ -3,11 +3,13 @@ new Vue({
     data: {
         message: 'whats uppp',
         value: 10,
+        name: 'anmol',
         coords: {
             x: 0,
             y: 0
 
-        }
+        },
+
     },
     methods: {
         greet() {
@@ -18,6 +20,9 @@ new Vue({
         logCoords(e) {
             this.coords.x = e.offsetX
             this.coords.y = e.offsetY
+        },
+        updateName(e) {
+            console.log(e.target.value)
         }
     }
 
