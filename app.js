a8e232d27dabffd7c576b3ebc605a365ac78a76c
AnmolPatil2 <anmol.patil004@gmail.com>
2019-05-18 21:48:22 +0530

    double binding

--- app.js
+++ app.js
@@ -1,29 +1,9 @@
 new Vue({
     el: '#app',
     data: {
-        message: 'whats uppp',
-        value: 10,
-        name: 'anmol',
-        coords: {
-            x: 0,
-            y: 0
-
-        },
-
+        name: 'anmol'
     },
-    methods: {
-        greet() {
-            this.value++
-        }, logEvent(e) {
-            console.log(e);
-        },
-        logCoords(e) {
-            this.coords.x = e.offsetX
-            this.coords.y = e.offsetY
-        },
-        updateName(e) {
-            console.log(e.target.value)
-        }
-    }
+    methods: {}
+
 
 })
